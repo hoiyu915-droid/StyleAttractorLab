9463887d76d53c2ff6645c4c5acf23cd9969fdfa
hoiyu915-droid <hoiyu915@gmail.com>
2026-08-12 00:34:04 +0800

    feat: extend the full attractor catalog

--- style_attractor_lab/__init__.py
+++ style_attractor_lab/__init__.py
@@ -3,6 +3,7 @@
 from .core import (
     LabValidationError,
     assemble_recipe,
+    catalog_records,
     create_run,
     summarize_scores,
     validate_repository,
@@ -11,10 +12,10 @@ from .core import (
 __all__ = [
     "LabValidationError",
     "assemble_recipe",
+    "catalog_records",
     "create_run",
     "summarize_scores",
     "validate_repository",
 ]
 
-__version__ = "0.1.0"
-
+__version__ = "0.2.0"
